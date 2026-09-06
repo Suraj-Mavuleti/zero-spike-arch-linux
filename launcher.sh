112c61458a67095031942bfe38a5edfa75beed16
Suraj Mavuleti <surajmavuleti@gmail.com>
2026-09-06 20:23:38 +0530

    Feat(v8): Visual Overhaul using CustomTkinter Modern UI Framework

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# ARCH LINUX NATIVE AUTO-UPDATER
 cd /home/suraj/.gemini/antigravity/scratch/god_tier_suite/zero-spike-arch-linux
 git pull origin main --quiet
-python3 zero_spike_gui.py
+/home/suraj/.gemini/antigravity/scratch/v8_env/bin/python3 zero_spike_gui.py
